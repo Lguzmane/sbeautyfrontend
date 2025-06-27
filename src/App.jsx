13716d8d8fdcf8b54789091d4f1e334fccc64054
Lguzmane <lorenaguzmanelgueta@gmail.com>
2025-06-26 20:38:50 -0400

    Corrige importación de footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Importar los componentes
 import Header from './components/header.jsx';
-import Footer from './components/Footer.jsx';
+import Footer from './components/footer.jsx';
 
 // Importar las páginas
 import Home from './pages/1.Home.jsx';
